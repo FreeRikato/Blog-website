1af0740b38b95ad860c55f4e36ce1553903e2a94
FreeRikato <aravinthanrc@gmail.com>
2024-12-02 05:37:56 +0530

    Fix the routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,13 @@ import { Signin_Page } from './pages/Signin.page';
 import { Blog_Page } from './pages/Blog.page';
 import { DisplayBlog } from './pages/DisplayBlog.page';
 import { PublishBlog } from './pages/PublishBlog.page';
+import { HomePage } from './pages/Home.page';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<>Home Page</>} />
+        <Route path="/" element={<HomePage/>} />
         <Route path="/signup" element={<Signup_Page />} />
         <Route path="/signin" element={<Signin_Page />} />
         <Route path="/blog" element={<Blog_Page />} />
